a1a5320a6855a5366b48903e8eada4c85cba32fa
Tianhao Zhang <zhangth1990@hotmail.com>
2022-12-12 20:29:25 -0700

    add the post section of the feed component

--- components/Feed.js
+++ components/Feed.js
@@ -1,7 +1,30 @@
 import { SparklesIcon } from "@heroicons/react/24/outline";
 import Input from "./Input";
+import Post from "./Post";
 
 export default function Feed() {
+    const posts = [
+        {
+            id: "1",
+            name: "Sahand Ghavidel",
+            username: "codewithsahand",
+            userImg:
+                "https://image.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg",
+            img: "https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8TmF0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+            text: "nice view!",
+            timestamp: "2 hours ago",
+        },
+        {
+            id: "2",
+            name: "Sahand Ghavidel",
+            username: "codewithsahand",
+            userImg:
+                "https://image.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg",
+            img: "https://images.unsplash.com/photo-1447752875215-b2761acb3c5d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8TmF0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+            text: "wow!",
+            timestamp: "2 days ago",
+        },
+    ];
     return (
         <div className="xl:ml-[370px] border-l border-r border-gray-200 xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
             <div className="flex py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
@@ -13,6 +36,9 @@ export default function Feed() {
                 </div>
             </div>
             <Input />
+            {posts.map((post) => (
+                <Post key={post.id} post={post} />
+            ))}
         </div>
     );
 }
